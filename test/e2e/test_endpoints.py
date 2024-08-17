ea8882bff5c2526977616bdc65905ded1ba3d53c
Keenan Graham <ksgraham@stanford.edu>
2024-08-16 17:39:26 -0700

    Update tests

--- test/e2e/test_endpoints.py
+++ test/e2e/test_endpoints.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_endpoints_test_api():
+def test_endpoints_test_search_simple():
     from igvf_client import IgvfApi
     from igvf_client.models.software import Software
     api = IgvfApi()
@@ -11,4 +11,57 @@ def test_endpoints_test_api():
     assert len(r.graph) == 1
     r = api.search(query='abc', type=['Software'], limit=1)
     assert len(r.graph) == 1
-    assert isinstance(r.graph[0].actual_instance, Software)
+    item = r.graph[0].actual_instance
+    assert isinstance(item, Software)
+    assert item.type == ['Software', 'Item']
+    assert 'software' in item.id
+    assert 'lab' in item.lab
+    assert 'award' in item.award
+    assert item.status == 'released'
+    assert item.uuid is not None
+    assert item.title is not None
+
+
+def test_endpoints_test_search_filters():
+    from igvf_client import IgvfApi
+    from igvf_client.models.software import Software
+    api = IgvfApi()
+    all_users = api.search(type=['User'], limit=0)
+    cherry_lab_users = api.search(
+        type=['User'],
+        limit=0,
+        field_filters={
+            'lab': '/labs/j-michael-cherry/'
+        }
+    )
+    assert cherry_lab_users.total > 10
+    assert all_users.total > cherry_lab_users.total
+
+
+def test_endpoints_test_collections():
+    from igvf_client import IgvfApi
+    api = IgvfApi()
+
+    file_id = '/sequence-files/IGVFFI1297OKBA/'
+    files = api.measurement_sets(files_id=[file_id]).graph[0].files
+    assert len(files) >= 1
+    assert file_id in files
+
+    users = api.users(lab=['/labs/j-michael-cherry/'])
+    assert users.total > 10
+    assert users.graph[0].lab == '/labs/j-michael-cherry/'
+
+
+def test_endpoints_test_schemas():
+    from igvf_client import IgvfApi
+    api = IgvfApi()
+
+    schemas = api.schemas()
+    assert len(schemas) > 50
+
+    assert 'AccessKey' in schemas
+    assert 'User' in schemas
+
+    user_schema = api.schema_for_item_type('User')
+    assert schemas['User'] == user_schema
+    assert False
