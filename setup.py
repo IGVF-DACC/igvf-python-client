d72c066f222b5b439cef7caaebe9b850864b9898
Keenan Graham <ksgraham@stanford.edu>
2024-08-22 17:28:57 -0700

    Change version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "igvf-client"
-VERSION = "46.1.0"
+VERSION = "46.1.0-beta"
 PYTHON_REQUIRES = ">=3.9"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
